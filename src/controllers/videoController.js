9b301aa659fbf6a0ecdb2ea5c3b2d7679c222ff0
AugWoo <103727168+AugWoo@users.noreply.github.com>
2022-10-26 15:37:10 +0900

    use static

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -33,15 +33,13 @@ export const postEdit = async (req, res) => {
   const { id } = req.params;
   const { title, description, hashtags} = req.body;
   const video = await Video.exist({ _id: id });
-  if(!video) {
+  if(!video) { 
     return res.render('404', { pageTitle: 'Video not found.' });
   }
   await Video.findByIdAndUpdate(id, {
     title,
     description,
-    hashtags: hashtags
-      .split(',')
-      .map(word => word.startsWith('#') ? word : `#${word}`),
+    hashtags: Video.formatHashtags(hashtags),
   });
   await video.save();
   return res.redirect(`/videos/${id}`);
@@ -57,11 +55,7 @@ export const postUpload = async(req, res) => {
     await Video.create({
       title,
       description,
-      hashtags: hashtags.split(',').map(word => `#${word}`),
-      meta: {
-        views: 0,
-        rating: 0,
-      },
+      hashtags: Video.formatHashtags(hashtags),
     });
     return res.redirect('/');
   } catch(err) {
