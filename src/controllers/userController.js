cd9f0aa8ce3c4259c74a67915d7cc54ef44c2c64
AugWoo <103727168+AugWoo@users.noreply.github.com>
2022-11-04 20:27:57 +0900

    cookie & session

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -59,6 +59,8 @@ export const postLogin = async (req, res) => {
       errorMessage: 'Wrong password.',
     });
   }
+  req.session.loggedIn = true;
+  req.session.user = user;
   console.log('login!');
   return res.redirect('/');
 };
--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import morgan from 'morgan'; // HTTP request logger middleware.
+import session from 'express-session';
+import MongoStore from 'connect-mongo';
 import rootRouter from './routers/rootRouter';
 import userRouter from './routers/userRouter';
 import videoRouter from './routers/videoRouter';
+import { localsMiddleware } from './middlewares';
 
 const app = express();
 const logger = morgan('dev');
@@ -10,9 +13,19 @@ const logger = morgan('dev');
 app.set('view engine', 'pug');
 app.set('views', process.cwd() + '/src/views');
 app.use(logger);
-app.use(express.urlencoded({ extended: true })); 
-  // this helps express app understand value of form.
-  // express can't read data by using form.
+app.use(express.urlencoded({ extended: true }));
+// this helps express app understand value of form.
+// express can't read data by using form.
+app.use(
+  session({
+    secret: 'hello!',
+    resave: true,
+    saveUninitialized: true,
+    store: MongoStore.create({ mongoUrl: 'mongodb://127.0.0.1:27017/wootube' }),
+  })
+);
+
+app.use(localsMiddleware);
 app.use('/', rootRouter);
 app.use('/videos', videoRouter);
 app.use('/users', userRouter);
