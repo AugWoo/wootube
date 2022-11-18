a646d4374446ec47eac9b3261ab98d4005c9af8f
AugWoo <103727168+AugWoo@users.noreply.github.com>
2022-11-18 12:16:34 +0900

    remove comments

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,16 +1,18 @@
 const videoContainer = document.querySelector('#videoContainer');
 const form = document.querySelector('#commentForm');
+const deleteComments = document.querySelectorAll('#deleteBtn');
 
 const addComment = (text, id) => {
   const videoComments = document.querySelector('.video__comments ul');
   const newComment = document.createElement('li');
-  newComment.dataset.id = id;
-  newComment.classList.add('video__comment');
+  newComment.dataset.commentId = id;
+  newComment.className = 'video__comment';
   const icon = document.createElement('i');
   icon.className = 'fas fa-comment';
   const span = document.createElement('span');
   span.innerText = `${text}`;
   const span2 = document.createElement('span');
+  span2.id = 'deleteBtn';
   span2.innerText = '❌';
   newComment.appendChild(icon);
   newComment.appendChild(span);
@@ -27,7 +29,7 @@ const handleSubmit = async (e) => {
     alert('Not vacuum.');
     return;
   }
-  const { response } = await fetch(`/api/videos/${videoId}/comment`, {
+  const response = await fetch(`/api/videos/${videoId}/comment`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -36,13 +38,28 @@ const handleSubmit = async (e) => {
       text,
     }),
   });
-  textarea.value = '';
-  const { newCommentId } = await response.json();
-  if (response === 201) {
+  if (response.status === 201) {
+    textarea.value = '';
+    const { newCommentId } = await response.json();
     addComment(text, newCommentId);
   }
 };
 
 if (form) {
   form.addEventListener('submit', handleSubmit);
+}
+
+const handleDeleteComment = async (e) => {
+  const li = e.srcElement.parentNode;
+  const { commentId } = li.dataset;
+  await fetch(`/api/comments/${commentId}/delete`, {
+    method: 'DELETE',
+  });
+  li.remove();
+};
+
+if (deleteComments) {
+  deleteComments.forEach((deleteComment) => {
+    deleteComment.addEventListener('click', handleDeleteComment);
+  });
 }
--- src/models/User.js
+++ src/models/User.js
@@ -3,7 +3,7 @@ import bcrypt from 'bcrypt';
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
-  avatarUrl: String,
+  avatarUrl: { type: String, default: null },
   socialOnly: { type: Boolean, default: false },
   username: { type: String, required: true, unique: true },
   password: { type: String },
